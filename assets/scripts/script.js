e98b2aad8d3e2a2e17c9bdfb6a842936133ee04a
projectnum17 <egor.chornyy@gmail.com>
2025-09-22 12:21:37 +0300

    layout contact page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,11 +3,13 @@
 import { headerFlow as header } from './modules/headerFlow.js';
 import { productGallery as slider } from './modules/productGallery.js';
 import { feedBackForm as form } from './modules/feedBackForm.js';
+import mapHandler from './modules/mapHandler.js';
 import helpers from './modules/helpers.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     header();
     slider();
     form();
+    mapHandler();
     helpers();
 });
